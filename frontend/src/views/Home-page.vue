388b0647b2074e20bdfd5fb31aaafaf01af349a1
Nontapat Kupreecha <56498257+nonava172352@users.noreply.github.com>
2022-05-10 17:29:01 +0700

    Update

--- frontend/src/views/Home-page.vue
+++ frontend/src/views/Home-page.vue
@@ -1,7 +1,9 @@
 <template>
   <body>
-    <div>
-      <h1>asdas</h1>
+    <div class="text-center">
+      <b-img class="text-center" src="https://app.dtac.co.th/vaccine/assets/images/banner/WebBanner-desktop-1120x400-Th_2022-Apr-28.jpg" fluid alt="Responsive image"></b-img><br><br>
+      <h1>ดีแทค เปิดลงทะเบียน รับการฉีดวัคซีนโควิด-19</h1><br>
+      <a href="/register" class="btn btn-primary" role="button">ลงทะเบียนจองคิว - Register</a>
     </div>
   </body>
 </template>
